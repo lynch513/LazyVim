45f4b718d64de07105591c91097af36bbbeb0912
lynch513 <lynch513@yandex.ru>
2025-02-04 22:27:49 +0500

    added blink-cmp

---
+++ lua/plugins/blink-cmp.lua
@@ -0,0 +1,26 @@
+return {
+  "saghen/blink.cmp",
+  config = function()
+    local blink = require("blink.cmp")
+
+    blink.setup({
+      keymap = {
+        preset = "default",
+        ["<C-j>"] = { "select_next", "fallback" },
+        ["<C-k>"] = { "select_prev", "fallback" },
+        ["<Tab>"] = {
+          function(cmp)
+            if cmp.snippet_active() then
+              return cmp.accept()
+            else
+              return cmp.select_and_accept()
+            end
+          end,
+          "snippet_forward",
+          "fallback",
+        },
+        ["<S-Tab>"] = { "snippet_backward", "fallback" },
+      },
+    })
+  end,
+}
